c37be16f8cc8a00078fd02932ef25792b5ebcd95
Igor Mogilnitskiy <igmo71@outlook.com>
2024-09-16 18:10:33 +0300

    0001 Add postgres, pgadmin, seq to the docker-compose.

---
+++ Bibosio.ProductsModule/Domain/Option.cs
@@ -0,0 +1,13 @@
+﻿using Bibosio.Common;
+
+namespace Bibosio.ProductsModule.Domain
+{
+    public class Option : AppEntity
+    {
+        public required string Name { get; set; }
+        public required string ShortName { get; set; }
+        public int Priority { get; set; }
+        public bool IsUseShortName { get; set; }
+        public bool IsUseValue { get; set; }
+    }
+}
--- Bibosio.ProductsModule/Domain/Product.cs
+++ Bibosio.ProductsModule/Domain/Product.cs
@@ -6,5 +6,6 @@ namespace Bibosio.ProductsModule.Domain
     public class Product : AppEntity
     {
         public required SKU SKU { get; set; }
+        public List<Option>? ProductOptions { get; set; }
     }
 }
